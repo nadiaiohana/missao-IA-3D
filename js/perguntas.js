e121cac06458c654d45909e5f4a0ae7ee0f3deb9
nadiaiohana <130075110+nadiaiohana@users.noreply.github.com>
2024-08-21 12:18:35 -0300

    .

---
+++ js/perguntas.js
@@ -0,0 +1,61 @@
+const perguntas = [
+    {
+        enunciado: "Você apoio o racismo?",
+        alternativas: [
+            {
+                texto: "Sim, eu apoio o movimento racista",
+                afirmacao: [
+                    "Vocẽ realmente já ouviu falar o que é racismo? A sua história e quantas milhares de pessoas foram prejudicadas e mortas? ",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto: "Não, sou contra o movimento racista",
+                afirmacao: [
+                    " Que legal! concordamos que o  racismo não é uma coisa boa",
+                    "afirmacao 2"
+                    ]
+            }           
+            
+        ]
+    },
+    {
+        enunciado: "Principal dano causado pelo racismo",
+        alternativas: [
+            {
+                texto:"Fortalece a autoestima das vitimas",
+                afirmacao: [
+                    "Racismo enfraquece a autoestima, perpetuando sentimentos de inferioridade",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto: "Contribui para a exclusão social e marginalização de grupos minoridarios",
+                afirmacao: [
+                    "Reforça a desigualdade e esteriótipos discriminatórios",
+                    "afirmacao 2"
+                    ]
+            }
+        ]
+    },
+    {
+        enunciado: "O que caracteriza o racismo estrutural na sociedade?",
+        alternativas: [
+            {
+                texto:"Desigualdade raciais",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto:"Alternativa 2 da pergunta 3",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            }
+            
+        ]
+    },
+];
