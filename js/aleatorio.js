dbfbe907ddb3edf024df7b7c88ac728fb1c58de5
nadiaiohana <130075110+nadiaiohana@users.noreply.github.com>
2024-09-11 10:11:39 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,3 +1,8 @@
+const nomes = ["Guilherme", "Paulo", "Kaike", "Nadia", "Sandra, "Aline"]
+
 export function aleatorio (lista)
- const posicao = Math.floor(Math.random()" Lista.Lenght);
+  const posicao = Math.floor(Math.random()* Lista.Lenght);
+  return lista[posicao]; 
 }
+
+export const nome = aleatorio(nomes)
